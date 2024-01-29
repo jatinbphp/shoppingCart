4dc1da58cd12f300a30c53d0a9912d22c759eacf
jatinbphp <jatin.p.php@gmail.com>
2024-01-29 12:38:15 +0530

    New Changes.

--- resources/views/admin/user/profile_form.blade.php
+++ resources/views/admin/user/profile_form.blade.php
@@ -4,7 +4,7 @@
             <label class="control-label" for="name">Name <span class="text-red">*</span></label>
             {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
             @if ($errors->has('name'))
-                <span class="help-block">
+                <span class="text-danger">
                         <strong>{{ $errors->first('name')}}</strong>
                     </span>
             @endif
@@ -15,7 +15,7 @@
             <label class="control-label" for="email">Email <span class="text-red">*</span></label>
             {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'email']) !!}
             @if ($errors->has('email'))
-                <span class="help-block">
+                <span class="text-danger">
                 <strong>{{ $errors->first('email') }}</strong>
             </span>
             @endif
@@ -27,7 +27,7 @@
             <label class="control-label" for="inputPassword3">Password</label>
             <input type="password" placeholder="Password" id="password" name="password" class="form-control" >
             @if ($errors->has('password'))
-                <span class="help-block">
+                <span class="text-danger">
                 <strong>{{ $errors->first('password') }}</strong>
             </span>
             @endif
@@ -38,7 +38,7 @@
             <label class="control-label" for="inputPassword3">Confirm Password</label>
             <input type="password" placeholder="Confirm password" id="password-confirm" name="password_confirmation" class="form-control" >
             @if ($errors->has('password_confirmation'))
-                <span class="help-block">
+                <span class="text-danger">
              <strong>{{ $errors->first('password_confirmation') }}</strong>
             </span>
             @endif
@@ -46,14 +46,14 @@
     </div>
     <div class="col-md-6">
         <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
-            <label class="col-md-12 control-label" for="image">Image<span class="text-red">*</span></label>
+            <label class="col-md-12 control-label" for="image">Image</label>
             <div class="col-md-12">
                 <div class="fileError">
                     {!! Form::file('image', ['class' => '', 'id'=> 'image','accept'=>'image/*', 'onChange'=>'AjaxUploadImage(this)']) !!}
                 </div>
                 <img id="DisplayImage" @if(!empty($user['image'])) src="{{ url($user['image'])}}" style="margin-top: 1%; padding-bottom:5px; display: block;" @else src="" style="padding-bottom:5px; display: none;" @endif width="150">
                 @if ($errors->has('image'))
-                    <span class="help-block">
+                    <span class="text-danger">
                     <strong>{{ $errors->first('image') }}</strong>
                 </span>
                 @endif
