499406a510089c801c5c6fc1ec723875cebd407f
jatinbphp <jatin.p.php@gmail.com>
2024-01-29 12:46:33 +0530

    add new columns.

--- database/migrations/2024_01_29_044704_create_categories_table.php
+++ database/migrations/2024_01_29_044704_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->nullable();
+            $table->integer('parent_category_id')->default(0);
             $table->string('name');
             $table->string('status');
             $table->string('image');
