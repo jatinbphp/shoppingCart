c102dbcba79ea6225e59140d8a8bc36daf6615ce
Prakash <prakash.v.php@gmail.com>
2024-02-02 19:04:18 +0530

    changes

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -28,7 +28,7 @@
                         <div class="card-header">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <a href="{{ route('category.create') }}"class="btn btn-default">Add New</a>
+                                    <a href="{{ route('category.create') }}" class="btn btn-info float-right">Add New</a>
                                 </div>
                             </div>
                         </div>
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -28,7 +28,7 @@
                         <div class="card-header">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <a href="{{ route('users.create') }}"class="btn btn-default">Add New</a>
+                                    <a href="{{ route('users.create') }}" class="btn btn-info float-right">Add New</a>
                                 </div>
                             </div>
                         </div>
