17b3046d8c4f04e6adf44f90efea14de22f41a6f
Jatin Bhatt <jatin.b.php@gmail.com>
2024-02-12 16:08:32 +0530

    order view

--- resources/views/admin/order/show_modal.blade.php
+++ resources/views/admin/order/show_modal.blade.php
@@ -9,8 +9,6 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Order Details</div>
-
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-4">
