5c3604d92d3edd7410d3329b680b02603be0b576
Jatin Bhatt <jatin.b.php@gmail.com>
2024-02-21 13:56:22 +0530

    changes

--- resources/views/admin/common/action-buttons.blade.php
+++ resources/views/admin/common/action-buttons.blade.php
@@ -1,4 +1,4 @@
-@if($section_name != 'contactus' && $section_name != 'cart_products' && $section_name != 'order_products' && !isset($order_dashboard))
+@if (!in_array($section_name, ['contactus', 'cart_products', 'order_products']) && !isset($order_dashboard))
     <div class="btn-group btn-group-sm">
         <a href="{{ url('admin/'.$section_name.'/'.$id.'/edit') }}" title="Edit {{$section_title}}" class="btn btn-sm btn-info tip ">
             <i class="fa fa-edit"></i>
@@ -6,7 +6,7 @@
     </div>
 @endif
 
-@if($section_name != 'content' && $section_name != 'cart_products' && $section_name != 'order_products' && !isset($order_dashboard))
+@if (!in_array($section_name, ['content', 'cart_products', 'order_products']) && !isset($order_dashboard))
     <span data-toggle="tooltip" title="Delete {{$section_title}}" data-trigger="hover">
         <button class="btn btn-sm btn-danger deleteRecord" data-id="{{$id}}" type="button" data-url="{{ url('admin/'.$section_name.'/'.$id) }}" data-section="{{$section_name}}_table">
             <i class="fa fa-trash"></i>
@@ -14,13 +14,13 @@
     </span>
 @endif
 
-@if($section_name == 'orders')
+@if ($section_name === 'orders')
     <div class="btn-group btn-group-sm">
         <a href="javascript:void(0)" title="View {{$section_title}}" data-id="{{$id}}" class="btn btn-sm btn-warning tip  order-info" data-url="{{ route('orders.show', ['order' => $id]) }}">
             <i class="fa fa-eye"></i>
         </a>
     </div>
-@elseif($section_name != 'cart_products' && $section_name != 'order_products')
+@elseif (!in_array($section_name, ['cart_products', 'order_products']))
     <div class="btn-group btn-group-sm">
         <a href="javascript:void(0)" title="View {{$section_title}}" data-id="{{$id}}" class="btn btn-sm btn-warning tip  view-info" data-url="{{ route($section_name.'.show', [$section_name != 'contactus' ? strtolower(str_replace(' ', '_', $section_title)) : 'contactu' => $id]) }}">
             <i class="fa fa-eye"></i>
@@ -28,7 +28,7 @@
     </div>
 @endif
 
-@if($section_name == 'cart_products' || $section_name == 'order_products')
+@if (in_array($section_name, ['cart_products', 'order_products']))
     <span data-toggle="tooltip" title="Delete {{$section_title}}" data-trigger="hover">
         <button class="btn btn-sm btn-danger deleteCartRecord" data-id="{{$id}}" type="button">
             <i class="fa fa-trash"></i>
