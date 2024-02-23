14843b12475f5568c28a98552fcc92f334086912
Jatin Bhatt <jatin.b.php@gmail.com>
2024-02-23 14:09:07 +0530

    changes

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -23,64 +23,62 @@ class ReportController extends Controller
             if (!empty($dateRange) && strpos($dateRange, ' - ') !== false) {
                 [$dateStart, $dateEnd] = explode(' - ', $dateRange);
             }
-            
             $usersWithOrdersAndProducts = User::where('role', 'user')->whereHas('orders')
             ->when($request->input('user_id'), function ($query, $userId) {
-                return $query->where('id', $userId);
-            })
-            ->whereHas('orders', function ($query) use ($request) {
-                if ($request->input('status')) {
-                    $query->where('status', $request->input('status'));
-                }
-            })
-            ->whereHas('orders', function ($query) use ($dateStart, $dateEnd) {
-                if (!is_null($dateStart)) {
-                    $query->whereDate('created_at', '>=', $dateStart);
-                }
-                if (!is_null($dateEnd)) {
-                    $query->whereDate('created_at', '<=', $dateEnd);
-                }
-            })
-            ->withCount([
-                'orders' => function ($query) use ($request, $dateStart, $dateEnd) {
-                    if ($request->input('status')) {
-                        $query->where('status', $request->input('status'));
-                    }
-                    if (!is_null($dateStart)) {
-                        $query->whereDate('orders.created_at', '>=', $dateStart);
-                    }
-                    if (!is_null($dateEnd)) {
-                        $query->whereDate('orders.created_at', '<=', $dateEnd);
-                    }
-                },
-                'orderItems' => function ($query) use ($request, $dateStart, $dateEnd) {
-                    if ($request->input('status')) {
-                        $query->where('status', $request->input('status'));
-                    }
-                    if (!is_null($dateStart)) {
-                        $query->whereHas('order', function ($query) use ($dateStart) {
-                            $query->whereDate('created_at', '>=', $dateStart);
-                        });
-                    }
-                    if (!is_null($dateEnd)) {
-                        $query->whereHas('order', function ($query) use ($dateEnd) {
-                            $query->whereDate('created_at', '<=', $dateEnd);
-                        });
-                    }
-                },
-                'orders as total_amount_sum' => function ($query) use ($request, $dateStart, $dateEnd) {
-                    if ($request->input('status')) {
-                        $query->where('status', $request->input('status'));
-                    }
-                    if (!is_null($dateStart)) {
-                        $query->whereDate('orders.created_at', '>=', $dateStart);
-                    }
-                    if (!is_null($dateEnd)) {
-                        $query->whereDate('orders.created_at', '<=', $dateEnd);
-                    }
-                    $query->select(DB::raw('coalesce(sum(total_amount), 0) as total_amount_sum'));
-                }
-            ]);
+              return $query->where('id', $userId);
+          })
+          ->whereHas('orders', function ($query) use ($request, $dateStart, $dateEnd) {
+              if ($request->input('status')) {
+                  $query->where('status', $request->input('status'));
+              }
+              if (!is_null($dateStart)) {
+                  $query->whereDate('created_at', '>=', $dateStart);
+              }
+              if (!is_null($dateEnd)) {
+                  $query->whereDate('created_at', '<=', $dateEnd);
+              }
+          })
+          ->withCount([
+              'orders' => function ($query) use ($request, $dateStart, $dateEnd) {
+                  if ($request->input('status')) {
+                      $query->where('status', $request->input('status'));
+                  }
+                  if (!is_null($dateStart)) {
+                      $query->whereDate('created_at', '>=', $dateStart);
+                  }
+                  if (!is_null($dateEnd)) {
+                      $query->whereDate('created_at', '<=', $dateEnd);
+                  }
+              },
+              'orderItems' => function ($query) use ($request, $dateStart, $dateEnd) {
+                  if ($request->input('status')) {
+                      $query->where('status', $request->input('status'));
+                  }
+                  if (!is_null($dateStart)) {
+                      $query->whereHas('order', function ($query) use ($dateStart) {
+                          $query->whereDate('created_at', '>=', $dateStart);
+                      });
+                  }
+                  if (!is_null($dateEnd)) {
+                      $query->whereHas('order', function ($query) use ($dateEnd) {
+                          $query->whereDate('created_at', '<=', $dateEnd);
+                      });
+                  }
+              },
+              'orders as total_amount_sum' => function ($query) use ($request, $dateStart, $dateEnd) {
+                  if ($request->input('status')) {
+                      $query->where('status', $request->input('status'));
+                  }
+                  if (!is_null($dateStart)) {
+                      $query->whereDate('created_at', '>=', $dateStart);
+                  }
+                  if (!is_null($dateEnd)) {
+                      $query->whereDate('created_at', '<=', $dateEnd);
+                  }
+                  $query->select(DB::raw('coalesce(sum(total_amount), 0) as total_amount_sum'));
+              }
+          ]);
+      
                 
             return Datatables::of($usersWithOrdersAndProducts)
                 ->addIndexColumn()
@@ -108,38 +106,85 @@ class ReportController extends Controller
         $data['menu'] = 'Products Purchased Report';
 
         $collection = Products::join('order_items', 'products.id', '=', 'order_items.product_id')
-                        ->join('orders', 'orders.id', '=', 'order_items.order_id')
-                        ->select(
-                            'products.*',
-                            DB::raw('SUM(order_items.product_qty) as total_quantity'),
-                            DB::raw('SUM(order_items.sub_total) as total_amount')
-                        )
-                        ->when($request->input('status'), function ($query, $status) {
-                            $query->where('orders.status', $status);
-                        })
-                        ->when($request->input('product_id'), function ($query, $product_id) {
-                            $query->where('products.id', $product_id);
-                        })
-                        ->when($request->input('daterange'), function ($query, $daterange) {
-                            $dates = explode("-", $daterange);
-                            if (count($dates) == 2) {
-                                $start_date = date('Y-m-d', strtotime(trim($dates[0])));
-                                $end_date = date('Y-m-d', strtotime(trim($dates[1])));
-                                $query->whereDate('orders.created_at', '>=', $start_date)
-                                    ->whereDate('orders.created_at', '<=', $end_date);
-                            }
-                        })
-                        ->groupBy('products.id')
-                        ->withCount([
-                            'orderItems',
-                            'orderItems as product_price_sum' => function ($query) {
-                                $query->select(DB::raw('coalesce(sum(sub_total), 0) as product_price_sum'));
-                            },
-                            'orderItems as product_qty_sum' => function ($query) {
-                                $query->select(DB::raw('coalesce(sum(product_qty), 0) as product_qty_sum'));
-                            }
-                        ]);
-
+        ->join('orders', 'orders.id', '=', 'order_items.order_id')
+        ->select(
+            'products.*',
+            DB::raw('SUM(order_items.product_qty) as total_quantity'),
+            DB::raw('SUM(order_items.sub_total) as total_amount')
+        ) 
+        ->when($request->input('status') && $request->input('daterange'), function ($query) use ($request) {
+            $query->where('orders.status', $request->input('status'))
+                  ->whereBetween('orders.created_at', [
+                      date('Y-m-d 00:00:00', strtotime(explode('-', $request->input('daterange'))[0])),
+                      date('Y-m-d 23:59:59', strtotime(explode('-', $request->input('daterange'))[1]))
+                  ]);
+        })
+        ->when($request->input('status') && !$request->input('daterange'), function ($query) use ($request) {
+            $query->where('orders.status', $request->input('status'));
+        })
+        ->when(!$request->input('status') && $request->input('daterange'), function ($query) use ($request) {
+            $dates = explode("-", $request->input('daterange'));
+            if (count($dates) == 2) {
+                $start_date = date('Y-m-d', strtotime(trim($dates[0])));
+                $end_date = date('Y-m-d', strtotime(trim($dates[1])));
+                $query->whereDate('orders.created_at', '>=', $start_date)
+                      ->whereDate('orders.created_at', '<=', $end_date);
+            }
+        })
+        ->when($request->input('product_id'), function ($query, $product_id) {
+            $query->where('products.id', $product_id);
+        })
+        ->groupBy('products.id')
+        ->withCount([
+            'orderItems',
+            'orderItems as product_price_sum' => function ($query) use ($request) {
+                $query->select(DB::raw('coalesce(sum(sub_total), 0) as product_price_sum'))
+                    ->join('orders', 'orders.id', '=', 'order_items.order_id')
+                    ->when($request->input('status') && $request->input('daterange'), function ($query) use ($request) {
+                        $query->where('orders.status', $request->input('status'))
+                              ->whereBetween('orders.created_at', [
+                                  date('Y-m-d 00:00:00', strtotime(explode('-', $request->input('daterange'))[0])),
+                                  date('Y-m-d 23:59:59', strtotime(explode('-', $request->input('daterange'))[1]))
+                              ]);
+                    })
+                    ->when($request->input('status') && !$request->input('daterange'), function ($query) use ($request) {
+                        $query->where('orders.status', $request->input('status'));
+                    })
+                    ->when(!$request->input('status') && $request->input('daterange'), function ($query) use ($request) {
+                        $dates = explode("-", $request->input('daterange'));
+                        if (count($dates) == 2) {
+                            $start_date = date('Y-m-d', strtotime(trim($dates[0])));
+                            $end_date = date('Y-m-d', strtotime(trim($dates[1])));
+                            $query->whereDate('orders.created_at', '>=', $start_date)
+                                  ->whereDate('orders.created_at', '<=', $end_date);
+                        }
+                    });
+            },
+            'orderItems as product_qty_sum' => function ($query) use ($request) {
+                $query->select(DB::raw('coalesce(sum(product_qty), 0) as product_qty_sum'))
+                    ->join('orders', 'orders.id', '=', 'order_items.order_id')
+                    ->when($request->input('status') && $request->input('daterange'), function ($query) use ($request) {
+                        $query->where('orders.status', $request->input('status'))
+                              ->whereBetween('orders.created_at', [
+                                  date('Y-m-d 00:00:00', strtotime(explode('-', $request->input('daterange'))[0])),
+                                  date('Y-m-d 23:59:59', strtotime(explode('-', $request->input('daterange'))[1]))
+                              ]);
+                    })
+                    ->when($request->input('status') && !$request->input('daterange'), function ($query) use ($request) {
+                        $query->where('orders.status', $request->input('status'));
+                    })
+                    ->when(!$request->input('status') && $request->input('daterange'), function ($query) use ($request) {
+                        $dates = explode("-", $request->input('daterange'));
+                        if (count($dates) == 2) {
+                            $start_date = date('Y-m-d', strtotime(trim($dates[0])));
+                            $end_date = date('Y-m-d', strtotime(trim($dates[1])));
+                            $query->whereDate('orders.created_at', '>=', $start_date)
+                                  ->whereDate('orders.created_at', '<=', $end_date);
+                        }
+                    });
+            }
+        ]);
+        
         if ($request->ajax()) {
             return Datatables::of($collection)
                 ->addIndexColumn()
